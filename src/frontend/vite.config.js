43ca3ef4da81f1456d62bdf7a54f01619efb30d6
Wouter Devinck <wouter.devinck@gmail.com>
2023-10-25 22:34:17 +0200

    Wip PWA support

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,10 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  plugins: [vue()],
+  plugins: [
+  
+    vue(),
+  
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "Homesmart"
+      }
+    })
+  
+  ],
 
   server: {
     port: 3000,
